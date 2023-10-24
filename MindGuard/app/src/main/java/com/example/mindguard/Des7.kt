54a8cafad6480ca00a03992732810c2375933463
Dennis Nzioki <89406534+DNXEMPIRE-1@users.noreply.github.com>
2023-10-24 05:06:58 +0300

    Add files via upload

---
+++ MindGuard/app/src/main/java/com/example/mindguard/Des7.kt
@@ -0,0 +1,11 @@
+package com.example.mindguard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Des7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_des7)
+    }
+}
